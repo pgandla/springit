1480f767a03386e146afab334d2e65670084aa4d
pgandla <pradeepdoit@gmail.com>
2021-12-30 15:35:25 +0530

    spring-boot chap4

---
+++ src/test/java/com/codezone/sprbtrestdemo/SprbtRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codezone.sprbtrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprbtRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
